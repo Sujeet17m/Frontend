5ff0a8861cfa06753a7a024999c11685f9f3e207
Sujeet17m <sujeet17das@gmail.com>
2025-08-30 20:25:17 +0530

    h1

--- learnReact/src/App.jsx
+++ learnReact/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
+import SecondPage from './Components/SecondPage';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, React!</h1>
       <Navbar />
+      <SecondPage />
+
     </div>
     
   )
---
+++ learnReact/src/Components/SecondPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SecondPage() {
+  return (
+    <div>
+      <h1><h1><h1>(ohh)</h1></h1></h1>
+    </div>
+  )
+}
+
+export default SecondPage
