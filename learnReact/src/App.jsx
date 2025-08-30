e85d299ed4a442b7738f7a097ddb46469d77a6f8
Sujeet17m <sujeet17das@gmail.com>
2025-08-30 20:16:44 +0530

    created first component

--- learnReact/src/App.jsx
+++ learnReact/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, React!</h1>
+      <Navbar />
     </div>
+    
   )
 }
 
-export default App
+export default App;
---
+++ learnReact/src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';  
+function Navbar() {
+  return (
+    <nav>
+      <h1>My Navbar</h1>
+    </nav>
+  );
+}
+export default Navbar;
